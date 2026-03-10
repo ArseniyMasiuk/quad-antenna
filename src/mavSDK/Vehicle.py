1661e4ecb0a74e466318728e3a54a05c70056253
Arsenii Masiuk <arsenii.masiuk@symphony-solutions.com>
2026-03-10 14:20:14 +0200

    added some of the fields to the ui and underlying backend

--- src/mavSDK/Vehicle.py
+++ src/mavSDK/Vehicle.py
@@ -1,15 +1,20 @@
-from mavsdk import System
 import asyncio
+from mavsdk import System
+from mavsdk.offboard import PositionNedYaw, OffboardError
 
 class Vehicle:
     def __init__(self):
         self.drone = System()
         self.lat = 49.84814986011828
         self.lon = 24.03920928224038
+        self.heading = 0
         self.is_connected = False
 
         self.watch_connection_task = None
         self.watch_gps_task = None
+        self.watch_heading_task = None
+
+        self.set_heading_ned_task = None
 
     async def connect(self, connection_string = 'tcp://127.0.0.1:5762'):
         print("Connecting to MAVLink...")
@@ -20,10 +25,13 @@ class Vehicle:
 
         self.watch_connection_task = asyncio.create_task(self.watch_connection())
         self.watch_gps_task = asyncio.create_task(self.watch_gps())
+        self.watch_heading_task = asyncio.create_task(self.watch_heading())
 
     async def disconnect(self):
         await self.close_task(self.watch_connection_task)
         await self.close_task(self.watch_gps_task)
+        await self.close_task(self.watch_heading_task)
+        await self.close_task(self.set_heading_ned_task)
 
         self.drone = None
         self.is_connected = False
@@ -32,14 +40,13 @@ class Vehicle:
         self.lon = 24.03920928224038
         self.is_connected = False
 
-    async def close_task(self, task):
-        if task is not None:
-            task.cancel()
-            try:
-                await task
-            except asyncio.CancelledError:
-                pass
-            task = None
+
+    def set_heading_ned(self, new_heading):
+        self.set_heading_ned_task = asyncio.create_task(self.__set_heading(new_heading))
+
+    async def watch_heading(self):
+        async for heading in self.drone.telemetry.heading():
+            self.heading = heading.heading_deg
 
     async def watch_gps(self):
         try:
@@ -52,3 +59,24 @@ class Vehicle:
     async def watch_connection(self):
         async for state in self.drone.core.connection_state():
             self.is_connected = state.is_connected
+
+    async def __set_heading(self, new_heading):
+        async for ned in self.drone.telemetry.position_velocity_ned():
+            p = ned.position
+            try:
+                await self.drone.offboard.set_position_ned(
+                    PositionNedYaw(p.north_m, p.east_m, p.down_m, float(new_heading)))
+                return ""
+            except OffboardError as e:
+                return f'Offboard Error: {e.__str__()}'
+            except Exception as e:
+                return f"Unknown Error: {e}"
+
+    async def close_task(self, task):
+        if task is not None:
+            task.cancel()
+            try:
+                await task
+            except asyncio.CancelledError:
+                pass
+            task = None
--- src/mavSDK/main.py
+++ src/mavSDK/main.py
@@ -2,9 +2,7 @@ import asyncio
 from nicegui import ui
 from Vehicle import Vehicle
 
-is_connected = False
 
-vehicle = Vehicle()
 
 def log(message):
      ui.notify(message)
@@ -12,17 +10,28 @@ def log(message):
 
 class GUIApp:
     def __init__(self):
-        self.map_view = None
+        self.vehicle = Vehicle()
         self.connection = {'proto': None, 'ip': None, 'port': None}
         self.button_connect = None
+        self.button_is_connected = False
+        self.status_badge = None
+
+        self.map_view = None
         self.marker_drone = None
+
+        self.current_headin = None
+        self.heading_badge = None
+        self.new_heading = None
+        self.button_set_heading = None
 
     def buidl_ui(self):
         ui.label('MAVLink Quad-Tracker').classes('text-h4 q-ma-md')
 
         # connetion 
         with ui.row().classes('w-full items-center gap-4'):
-            # Label передається як іменований аргумент
+            self.status_badge = ui.badge('Waiting...')
+            ui.timer(0.5, self.update_status)
+
             self.connection['proto'] = ui.select(['tcp', 'udp'], label='Protocol', value='tcp').classes('w-32')
             self.connection['ip'] = ui.input(label='IP Address', placeholder='10.1.1.50').classes('flex-grow')
             self.connection['port'] = ui.input(label='Port', placeholder='14550').props('mask="#####"').classes('w-24')
@@ -30,29 +39,46 @@ class GUIApp:
             self.button_connect = ui.button('Connect', on_click=self.toggle_connection).props('elevated')
 
         # Map
-        self.map_view = ui.leaflet(center=(vehicle.lat, vehicle.lon), zoom=13, ).classes('w-full h-150')
-        self.marker_drone = self.map_view.marker(latlng=(vehicle.lat, vehicle.lon))
+        self.map_view = ui.leaflet(center=(self.vehicle.lat, self.vehicle.lon), zoom=13, ).classes('w-full h-150')
+        self.marker_drone = self.map_view.marker(latlng=(self.vehicle.lat, self.vehicle.lon))
         ui.timer(0.2, self.update_map)
 
+        with ui.row().classes('w-full items-center gap-4'):
+             self.heading_badge = ui.badge('Heading: 0')
+             ui.timer(0.5, self.update_heading_badge)
+
+             self.new_heading = ui.input(label='New heading').classes('flex-grow')
+             self.button_set_heading = ui.button('Set new heading', on_click=self.button_update_heading).props('elevated')
+
+    def update_heading_badge(self):
+            self.heading_badge.text = f'Heading: {int(self.vehicle.heading)}'
+
+    async def button_update_heading(self):
+        self.vehicle.set_heading_ned(self.new_heading.value)
+        log(f'New heading set to: {self.new_heading.value}')
+
     def update_map(self):
-        self.map_view.set_center((vehicle.lat, vehicle.lon))
+        self.map_view.set_center((self.vehicle.lat, self.vehicle.lon))
         if self.marker_drone:
-            self.marker_drone.move(vehicle.lat, vehicle.lon)
+            self.marker_drone.move(self.vehicle.lat, self.vehicle.lon)
 
     async def toggle_connection(self):
-        global is_connected
-        if not is_connected:
-            asyncio.get_event_loop().create_task(vehicle.connect(f"{self.connection['proto'].value}://{self.connection['ip'].value}:{self.connection['port'].value}"))
+        if not self.button_is_connected:
+            asyncio.get_event_loop().create_task(self.vehicle.connect(f"{self.connection['proto'].value}://{self.connection['ip'].value}:{self.connection['port'].value}"))
             log(f'Connecting via {self.connection['proto'].value} to {self.connection['ip'].value}:{self.connection['port'].value}')
-            is_connected = True
+            self.button_is_connected = True
             self.button_connect.set_text('Disconnect')
             self.button_connect.props('color=red')
         else:
-            await vehicle.disconnect()
-            is_connected = False
+            await self.vehicle.disconnect()
+            self.button_is_connected = False
             self.button_connect.set_text('Connect')
             self.button_connect.props('color=green')
 
+    def update_status(self):
+            self.status_badge.text = 'Connected' if self.vehicle.is_connected else 'Waiting...'
+            color = 'green' if self.vehicle.is_connected else 'red'
+            self.status_badge.props(f'color={color}')
 
 @ui.page('/')
 def main_page():
